40f5605845a342244b8628cc278e5a1076b5c4a1
Mario Hayashi <mario.hayashi@gmail.com>
2014-10-01 13:47:13 +0100

    Add model and run migration to create 'tasks' table. Display tasks in Tasks view.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
-  def index
+  def index # Looks automatically for index.html.erb view.
+    # Instance variable by prefixing with '@'
+    @tasks = Task.all
   end
 end
--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe "Tasks", :type => :feature do
 # describe "Tasks" do
   describe "GET /tasks" do
     it "display some tasks" do
+      # Create sample data as we're not using database.
+      @task = Task.create :task => 'go to bed'
       # Using capybara.
       visit tasks_path # Requires route to be defined.
-      page.should have_content 'go to bed'
+      expect(page).to have_content 'go to bed'
     end
   end
 end
